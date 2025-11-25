98e7fa33cbf40d959e5c966e168e238d4967c9df
ASmolenkov <a.smolenkov@list.ru>
2025-11-25 14:02:54 +0300

    Рефакторинг CurrencyMapper

--- src/main/java/by/smolenok/currencyexchange/mapper/CurrencyMapper.java
+++ src/main/java/by/smolenok/currencyexchange/mapper/CurrencyMapper.java
@@ -11,16 +11,21 @@ import java.sql.SQLException;
 @UtilityClass
 public class CurrencyMapper {
 
+    private static final String COLUMN_LABEL_ID = "id";
+    private static final String COLUMN_LABEL_FULL_NAME = "full_name";
+    private static final String COLUMN_LABEL_CODE = "code";
+    private static final String COLUMN_LABEL_SIGN = "sign";
+
     public CurrencyResponseDto toResponse(Currency currency) {
         return new CurrencyResponseDto(currency.getId(), currency.getName(), currency.getCode(), currency.getSign());
     }
 
     public Currency resultSetToCurrency(ResultSet resultSet) throws SQLException {
         return Currency.builder()
-                .id(resultSet.getInt("id"))
-                .name(resultSet.getString("full_name"))
-                .code(resultSet.getString("code"))
-                .sign(resultSet.getString("sign"))
+                .id(resultSet.getInt(COLUMN_LABEL_ID))
+                .name(resultSet.getString(COLUMN_LABEL_FULL_NAME))
+                .code(resultSet.getString(COLUMN_LABEL_CODE))
+                .sign(resultSet.getString(COLUMN_LABEL_SIGN))
                 .build();
 
     }
